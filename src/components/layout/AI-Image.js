4b9d46a848141560866a6b175d199905461f9031
Austin <austin.amaral3@gmail.com>
2024-02-12 17:47:44 -0800

    push

--- src/components/layout/AI-Image.js
+++ src/components/layout/AI-Image.js
@@ -52,35 +52,62 @@ export default function AI_Image() {
     
 
     return (
-        <div className="flex w-full" style={{height: 'calc(100vh - 100px)' }}>
-            <Sidebar />
-            <div className="flex flex-col mt-6 px-28">
+        <div className=" w-full" >
+            <div className='hidden sm:block'>
+                <Sidebar />
+            </div>
+            <div className="flex flex-col mt-6 px-6 sm:px-28">
                 <h1 className='ai-image-gen mb-2 '>AI Image Generator</h1>
-                <div className="flex flex-col mr-20">
-                    <p className='ai-image-description mb-8'>Enter a text description and generate an AI Image</p>
-                    <form onSubmit={generateImage} className="border rounded-sm border-gray-300 flex-grow flex items-center px-4">
+                <div className="flex flex-col ">
+                    <p className='ai-image-description mb-8 hidden sm:block'>Enter a text description and generate an AI Image</p>
+                    <form onSubmit={generateImage} className="hidden sm:flex border rounded-md border-gray-300 flex-grow items-center px-4">
                         <textarea
-                            rows='4'
-                            className="web-chat-default-text w-full outline-none resize-none p-2"
-                            value={inputText}
-                            onChange={(e) => setInputText(e.target.value)}
+                        rows='4'
+                        className="web-chat-default-text w-full outline-none resize-none p-2"
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
                         />
-                        <div className=''></div>
-                        {isLoading ? 'Generating...' : (
+                        {isLoading ? (
+                            <p className="ml-4">Generating...</p>
+                        ) : (
                             <button
-                            type="button"
-                            className="clone-bot-btn"
-                            onClick={() => generateImage()}
+                            type="submit"
+                            className="clone-bot-btn ml-4"
                             >
                             Generate
                             </button>
                         )}
                     </form>
+
+                    {/* Textarea only for mobile */}
+                    <div className="sm:hidden border rounded-md border-gray-300 flex-grow px-4">
+                        <textarea
+                        rows='4'
+                        className="web-chat-default-text w-full outline-none resize-none p-2"
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
+                        />
+                    </div>
+
+                    {/* Generate Button - Separate from textarea on mobile */}
+                    <div className="mt-2 sm:hidden">
+                        {isLoading ? (
+                            <p>Generating...</p>
+                        ) : (
+                            <button
+                            type="button"
+                            className="clone-bot-btn w-full "
+                            onClick={generateImage}
+                            >
+                            Generate
+                            </button>
+                        )}
+                    </div>
                 </div>
                 {generatedImage && ( // Check if generatedImage has a value
                     <div className="flex flex-col flex-grow bg-white border rounded-sm border-gray-300 mt-6 mr-20">
                         {/* Display the generated image */}
-                        <img src={generatedImage} alt="Generated" width={'600'} height={'400'} className="py-2 mt-4 px-24" />
+                        <Image src={generatedImage} alt="Generated" width={'600'} height={'400'} className="py-2 mt-4 px-24" />
                         <div className="flex justify-center">
                             {/* Download link */}
                             <Link href={generatedImage} download='generated_image.png' className="border border-gray-300 py-2 px-12 mb-2">
@@ -98,21 +125,21 @@ export default function AI_Image() {
                         href={'https://www.getbind.co/?utm_source=chat-app-bottom&utm_medium=chat-bot&utm_campaign=chat-bot'}
                     >
                         <span className='sidebar-how-text-hover px-1'>Created with </span>
-                        <Image src="/logo-black.svg" width={40} height={15} />
+                        <Image src="/logo-black.svg" alt='img' width={40} height={15} />
                     </Link>
                     <div className='flex-1'></div>
-                    <div className='flex description hidden sm:block disable-text'>
+                    <div className='description hidden sm:block disable-text'>
                         Bind can provide inaccurate information, including about people. Always double-check its answers. Your 
                         <Link href={''}>
                             <span className='privacy mx-1 sidebar-how-text-hover'>privacy</span>
                         </Link>
                         in Bind
                     </div>
-                    <div className='block sm:hidden disable-text'>
+                    {/* <div className='block sm:hidden disable-text'>
                         Always check its answers. 
                         <Link href={'/'} className='privacy'>privacy</Link>
                         in Bind
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,65 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
+import Bars3 from '../icons/Bars3';
+import { useState } from 'react';
 
 export default function Navbar() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <div className='navbar'>
             <div className='flex justify-between items-center py-3'>
-                <div className='flex'>
-                    <img src="/logo-black.svg" />
+                <div className='flex items-center'>
+                    <Link href={'https://app.getbind.co/webbot?utm_source=bind-image-generator&utm_medium=web&utm_campaign=bind-image-generator'}>
+                        <Image src="/logo-black.svg" alt='logo' height={26} width={81}/>
+                    </Link>
                 </div>
                 <div className='icon-border my-2 mx-3'></div>
                 <div className='web-chat-default-text'>AI Image Generator</div>
                 <div className='flex-1'></div>
-                <div className='hidden sm:block'>
+
+                <div className={`hidden sm:block ${isMobileMenuOpen ? 'hidden' : ''}`}>
                     <Link 
                         className="web-chat-default-text mr-5" 
-                        href={'https://app.getbind.co/login?utm_source=chat-app-top&utm_medium=chat-bot&utm_campaign=chat-bot'} 
+                        href={'https://app.getbind.co/register?utm_source=bind-image-generator&utm_medium=web&utm_campaign=bind-image-generator'} 
                         target='_blank'
                     >Create your own app</Link>
                     <Link 
                         className='mr-5 clone-bot-btn' 
-                        href={'https://www.getbind.co/?utm_source=chat-app-top&utm_medium=chat-bot&utm_campaign=chat-bot'} 
+                        href={'https://app.getbind.co/register?utm_source=bind-image-generator&utm_medium=web&utm_campaign=bind-image-generator'} 
                         target='_blank'
                     >Clone this bot</Link>
                 </div>
+                
+                <div className="lg:hidden flex items-center">
+                    <button className="outline-none mobile-menu-button" onClick={() => toggleMobileMenu((prev) => !prev)}>
+                        <svg className="w-6 h-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
+                </div>
+
+                {/* Mobile Menu - shown/hidden based on isMobileMenuOpen */}
+                {isMobileMenuOpen && (
+                    <div className="">
+                        {/* Mobile menu content here */}
+                        <Link 
+                            className="web-chat-default-text" 
+                            href={'https://app.getbind.co/register?utm_source=bind-image-generator&utm_medium=web&utm_campaign=bind-image-generator'} 
+                            target='_blank'
+                        >Create your own app</Link>
+                        <Link 
+                            className='clone-bot-btn' 
+                            href={'https://app.getbind.co/register?utm_source=bind-image-generator&utm_medium=web&utm_campaign=bind-image-generator'} 
+                            target='_blank'
+                        >Clone this bot</Link>
+                    </div>
+                )}
             </div>
         </div>
     )
